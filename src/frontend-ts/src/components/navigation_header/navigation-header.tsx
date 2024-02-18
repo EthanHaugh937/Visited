8c8297fcb02edcdbfd0621ecf00e97adf96854f8
44771 <ethanhaugh201@gmail.com>
2024-02-18 15:18:42 +0000

    Add Authenticator Wrapper; Logout

--- src/frontend-ts/src/components/navigation_header/navigation-header.tsx
+++ src/frontend-ts/src/components/navigation_header/navigation-header.tsx
@@ -1,25 +1,38 @@
 import { Divider } from "antd";
 import styles from "./navigation-header.module.css";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import awsconfig from "../../aws-exports";
+import { signOut } from "aws-amplify/auth";
+
+Amplify.configure(awsconfig);
+
+async function handleSignOut() {
+  await signOut()
+}
 
 function NavigationHeader() {
   return (
-    <div className={`${styles.header} bg-black-3`}>
-      <div className={`px-3 mt-2`}>LOGO</div>
-      <div className={`${styles.navElement} px-3 mt-2`}>
-        <nav className={styles.nav}>
-          <a href="/home" className={styles.authenticationText}>
-            Login
-          </a>
-          <Divider type="vertical" className={`${styles.divider} mx-3`} />
-          <a
-            href="/home"
-            className={`${styles.authenticationText} ${styles.signupPill} px-2 py-1`}
-          >
-            Sign Up
-          </a>
-        </nav>
+    <Authenticator className="mt-4">
+      <div className={`${styles.header} bg-black-3`}>
+        <div className={`px-3 mt-2`}>LOGO</div>
+        <div className={`${styles.navElement} px-3 mt-2`}>
+          <nav className={styles.nav}>
+            <a href="/home" className={styles.authenticationText}>
+              Login
+            </a>
+            <Divider type="vertical" className={`${styles.divider} mx-3`} />
+            <button
+              onClick={handleSignOut}
+              className={`${styles.authenticationText} ${styles.signupPill} px-2 py-1`}
+            >
+              Sign Out
+            </button>
+          </nav>
+        </div>
       </div>
-    </div>
+    </Authenticator>
   );
 }
 
