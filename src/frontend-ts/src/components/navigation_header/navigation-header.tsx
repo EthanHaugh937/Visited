fb8f2ee3a15e5e251c4525a057f6490f10e8f3a8
Ethan  Haugh <ethanhaugh201@gmail.com>
2024-02-19 16:56:48 +0000

    Added account drawer

--- src/frontend-ts/src/components/navigation_header/navigation-header.tsx
+++ src/frontend-ts/src/components/navigation_header/navigation-header.tsx
@@ -1,37 +1,62 @@
-import { Divider } from "antd";
+import { Button, Drawer, Row } from "antd";
 import styles from "./navigation-header.module.css";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import awsconfig from "../../aws-exports";
 import { signOut } from "aws-amplify/auth";
+import { UserOutlined } from "@ant-design/icons";
+import { useState } from "react";
 
 Amplify.configure(awsconfig);
 
-async function handleSignOut() {
-  await signOut()
-}
-
 function NavigationHeader() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const onDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
+  async function handleSignOut() {
+    await signOut();
+    setDrawerOpen(false)
+  }
+
+  const onDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
   return (
     <Authenticator className="mt-4">
       <div className={`${styles.header} bg-black-3`}>
-        <div className={`px-3 mt-2`}>LOGO</div>
-        <div className={`${styles.navElement} px-3 mt-2`}>
-          <nav className={styles.nav}>
-            <a href="/home" className={styles.authenticationText}>
-              Login
-            </a>
-            <Divider type="vertical" className={`${styles.divider} mx-3`} />
-            <button
-              onClick={handleSignOut}
-              className={`${styles.authenticationText} ${styles.signupPill} px-2 py-1`}
-            >
-              Sign Out
-            </button>
-          </nav>
+        <div className={`px-3 mt-2`}>Visited</div>
+        <div className={`px-3 mt-2`}>
+          <Button
+            ghost
+            shape="circle"
+            icon={<UserOutlined />}
+            onClick={onDrawerOpen}
+          ></Button>
         </div>
       </div>
+
+      <Drawer
+        onClose={onDrawerClose}
+        open={drawerOpen}
+        placement="top"
+        height={225}
+        closable={false}
+      >
+        <Row className="mt-3" justify={"center"}>
+          <Button type="text" size="large">
+            Account
+          </Button>
+        </Row>
+        <Row className="mt-3" justify={"center"}>
+          <Button type="text" size="large" onClick={handleSignOut}>
+            Logout
+          </Button>
+        </Row>
+      </Drawer>
     </Authenticator>
   );
 }
