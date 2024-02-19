b8cea7fb919a315eda00e26108be1db4275a46cb
Ethan  Haugh <ethanhaugh201@gmail.com>
2024-02-19 17:51:37 +0000

    Add Basic Routes

--- src/frontend-ts/src/components/app/App.tsx
+++ src/frontend-ts/src/components/app/App.tsx
@@ -1,7 +1,18 @@
 import NavigationHeader from "../navigation_header/navigation-header";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <NavigationHeader />;
+  return (
+    <>
+      <NavigationHeader />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<h1>Hello World</h1>} />
+          <Route path="/nothome" element={<h1>Goodbye World</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
