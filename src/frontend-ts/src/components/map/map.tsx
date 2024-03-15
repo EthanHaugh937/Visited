75888a3bc42de1513ee7ee89a87ec7e52bbde157
Ethan  Haugh <ethanhaugh201@gmail.com>
2024-03-15 13:17:03 +0000

    Highlight countries based on array of country codes

--- src/frontend-ts/src/components/map/map.tsx
+++ src/frontend-ts/src/components/map/map.tsx
@@ -4,6 +4,8 @@ import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
 import styles from "./map.module.css";
 import { useLayoutEffect } from "react";
 
+const data = ["US", "CA"];
+
 export function Map() {
   useLayoutEffect(() => {
     const root = am5.Root.new("chartdiv");
@@ -20,27 +22,38 @@ export function Map() {
     const polygonSeries = chart.series.push(
       am5map.MapPolygonSeries.new(root, {
         geoJSON: am5geodata_worldLow,
-        exclude: ["AQ"]
+        exclude: ["AQ"],
       })
     );
 
+    // Change the default colour for countries
     polygonSeries.mapPolygons.template.setAll({
       interactive: true,
       fill: am5.color(0xaaaaaa),
-      templateField: "polygonSettings"
+      templateField: "polygonSettings",
     });
 
+    // Highlight country on hover
     polygonSeries.mapPolygons.template.states.create("hover", {
-      fill: am5.color("#6c6765")
+      fill: am5.color("#6c6765"),
     });
-    
-    const polygonTemplate = polygonSeries.mapPolygons.template
 
-    polygonTemplate.events.on("click", function(ev) {console.log(ev.target.dataItem?.dataContext)})
+    const polygonTemplate = polygonSeries.mapPolygons.template;
+
+    // Set all visited countries to different colour
+    polygonSeries.data.setAll(
+      Array.from(data, (id) => {
+        return { id: id, polygonSettings: { fill: am5.color("#f00") } };
+      })
+    );
+
+    polygonTemplate.events.on("click", function (ev) {
+      console.log(ev.target.dataItem);
+    });
     return () => {
       root.dispose();
     };
-  }, []);
+  }, [data]);
 
   return <div id="chartdiv" className={`${styles.map} mt-2`}></div>;
 }
