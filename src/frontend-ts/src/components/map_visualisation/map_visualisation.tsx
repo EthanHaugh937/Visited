36f1de8771b0e49dc47effc619f2efde90aca512
Ethan  Haugh <ethanhaugh201@gmail.com>
2024-03-19 16:34:01 +0000

    Add drill down to countries

--- src/frontend-ts/src/components/map_visualisation/map_visualisation.tsx
+++ src/frontend-ts/src/components/map_visualisation/map_visualisation.tsx
@@ -1,14 +1,21 @@
 import * as am5 from "@amcharts/amcharts5";
 import * as am5map from "@amcharts/amcharts5/map";
 import am5geodata_worldLow from "@amcharts/amcharts5-geodata/worldLow";
+import am5geodata_continentsLow from "@amcharts/amcharts5-geodata/worldLow";
+import am5geodata_data_countries from "@amcharts/amcharts5-geodata/data/countries2";
+import am5themes_Animated from "@amcharts/amcharts5/themes/Animated"
 import styles from "./map.module.css";
 import { useLayoutEffect } from "react";
 
-const data = ["US", "CA"];
+const countriesVisited = ["US", "CA", "GB"];
 
 export function Map() {
   useLayoutEffect(() => {
     const root = am5.Root.new("chartdiv");
+
+    root.setThemes([
+      am5themes_Animated.new(root)
+    ]);
 
     let chart = root.container.children.push(
       am5map.MapChart.new(root, {
@@ -19,36 +26,96 @@ export function Map() {
       })
     );
 
-    const polygonSeries = chart.series.push(
+    const continentSeries = chart.series.push(
       am5map.MapPolygonSeries.new(root, {
-        geoJSON: am5geodata_worldLow,
+        geoJSON: am5geodata_continentsLow,
         exclude: ["AQ"],
       })
     );
 
+    const countrySeries = chart.series.push(
+      am5map.MapPolygonSeries.new(root, {
+        geoJSON: am5geodata_worldLow,
+        exclude: ["AQ"],
+        visible: false,
+      })
+    );
+
     // Change the default colour for countries
-    polygonSeries.mapPolygons.template.setAll({
+    continentSeries.mapPolygons.template.setAll({
       interactive: true,
+      tooltipText: "{name}",
       fill: am5.color(0xaaaaaa),
       templateField: "polygonSettings",
     });
 
     // Highlight country on hover
-    polygonSeries.mapPolygons.template.states.create("hover", {
+    continentSeries.mapPolygons.template.states.create("hover", {
       fill: am5.color("#6c6765"),
     });
 
-    const polygonTemplate = polygonSeries.mapPolygons.template;
+    countrySeries.mapPolygons.template.setAll({
+      tooltipText: "{name}",
+      fill: am5.color(0xaaaaaa),
+      interactive: true
+    });
+
+    countrySeries.mapPolygons.template.states.create("hover", {
+      fill: am5.color("#6c6765")
+    });
 
     // Set all visited countries to different colour
-    polygonSeries.data.setAll(
-      Array.from(data, (id) => {
+    continentSeries.data.setAll(
+      Array.from(countriesVisited, (id) => {
         return { id: id, polygonSettings: { fill: am5.color("#f00") } };
       })
     );
 
-    polygonTemplate.events.on("click", function (ev) {
-      console.log(ev.target.dataItem);
+    var data = [];
+    for (var id in am5geodata_data_countries) {
+      if (am5geodata_data_countries.hasOwnProperty(id)) {
+        var country = am5geodata_data_countries[id];
+        if (country.maps.length) {
+          data.push({
+            id: id,
+            map: country.maps[0],
+          });
+        }
+      }
+    }
+    countrySeries.data.setAll(data);
+
+    // Handle zoom to country and drill down
+    continentSeries.mapPolygons.template.events.on("click", function (ev) {
+      const dataItem = ev.target
+        .dataItem! as am5.DataItem<am5map.IMapPolygonSeriesDataItem>;
+      const data = dataItem?.dataContext as any;
+      const selectedMap = countrySeries.getDataItemById(data.id)
+        ?.dataContext as any;
+
+      // Zoom to country
+      const zoomContinentAnimation = continentSeries.zoomToDataItem(dataItem);
+      continentSeries.zoomToDataItem(dataItem);
+
+      if (selectedMap.map) {
+        Promise.all([
+          zoomContinentAnimation?.waitForStop(),
+          am5.net.load(
+            "https://cdn.amcharts.com/lib/5/geodata/json/" +
+              selectedMap.map +
+              ".json",
+            chart
+          ),
+        ]).then((results) => {
+          var geodata = am5.JSONParser.parse(results[1].response as string);
+          countrySeries.setAll({
+            geoJSON: geodata,
+          });
+
+          continentSeries.hide();
+          countrySeries.show();
+        });
+      }
     });
 
     return () => {
@@ -56,7 +123,7 @@ export function Map() {
     };
   }, []);
 
-  return <div id="chartdiv" className={`${styles.map}`} />
+  return <div id="chartdiv" className={`${styles.map}`} />;
 }
 
 export default Map;
