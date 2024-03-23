c960af11b6fb9e6e3c1c40af7d1a2c3d8c0f2ba5
Ethan Haugh <90157026+EthanHaugh@users.noreply.github.com>
2024-03-23 17:44:42 +0000

    Create statistics visualisations (#7)
    * Create statistics visualisations
    
    * Remove unnecessay CSS

--- src/frontend-ts/src/components/visited_statistics/visited_statistics.tsx
+++ src/frontend-ts/src/components/visited_statistics/visited_statistics.tsx
@@ -1,41 +1,45 @@
-import { Card, Carousel, Col, Row, Statistic } from "antd"
-import styles from './visited_statistics.module.css'
+import { Col, Row } from "antd";
+import styles from "./visited_statistics.module.css";
+import { StatisticVisualisation } from "../statistics/countries_visited/countries_visited";
+import {
+  numberOfUNCountries,
+  visitedCountriesPercentageCircleColours,
+  wishCountriesPercentageCircleColours,
+} from "../../const";
 
 export interface VisitedStatisticsProps {
-    visitedPlaces: Array<string>;
+  visitedPlaces: Array<string>;
 }
 
-export function VisitedStatistics({visitedPlaces}: VisitedStatisticsProps) {   
-    const visitedContinents = Array.from(visitedPlaces, (id) => {
-        return id.split("-")[0];
-      });
+export function VisitedStatistics({ visitedPlaces }: VisitedStatisticsProps) {
+  const visitedCountries = Array.from(visitedPlaces, (id) => {
+    return id.split("-")[0];
+  });
 
-    // Statistics container; resize depending on page size 
-    return (
-        <Row className={styles.statisticsContainer} wrap={false}>
-        <Col xs={24} sm={24} md={7} lg={7} xl={8} className="mx-2 mt-2">
-          <Card className={styles.card}>Test</Card>
-        </Col>
-        <Col xs={24} sm={24} md={8} lg={8} xl={8} className="mx-2 mt-2">
-          <Card className={styles.card}>
-            <Carousel>
-              <Statistic
-                title="Continents Visited"
-                value={visitedContinents.length}
-                suffix={"/ 7"}
-                className={styles.contentStyle}
-              />
-              <div>
-                <h3>2</h3>
-              </div>
-            </Carousel>
-          </Card>
-        </Col>
-        <Col xs={24} sm={24} md={7} lg={7} xl={7} className="mx-2 mt-2">
-          <Card className={styles.card}>Test</Card>
-        </Col>
-      </Row>
-    )
+  // Statistics container; resize depending on page size
+  return (
+    <Row className={styles.statisticsContainer} wrap={false}>
+      <Col xs={24} sm={24} md={7} lg={7} xl={8} className="mx-2 mt-2">
+        <div className={styles.card}>Test</div>
+      </Col>
+      <Col xs={24} sm={24} md={8} lg={8} xl={8} className="mx-2 mt-2">
+        <StatisticVisualisation
+          visualisationTitle="Visited Countries"
+          completeNumberOfCountries={visitedCountries.length}
+          maximumNumberOfCountries={numberOfUNCountries}
+          colourGradients={visitedCountriesPercentageCircleColours}
+        />
+      </Col>
+      <Col xs={24} sm={24} md={7} lg={7} xl={7} className="mx-2 mt-2">
+        <StatisticVisualisation
+          visualisationTitle="Wish List Countries Visited"
+          completeNumberOfCountries={visitedCountries.length}
+          maximumNumberOfCountries={6}
+          colourGradients={wishCountriesPercentageCircleColours}
+        />
+      </Col>
+    </Row>
+  );
 }
 
-export default VisitedStatistics
+export default VisitedStatistics;
---
+++ src/frontend-ts/src/const.ts
@@ -0,0 +1,15 @@
+import { ProgressProps } from "antd";
+
+export const numberOfUNCountries = 193;
+
+export const visitedCountriesPercentageCircleColours: ProgressProps["strokeColor"] =
+  {
+    "0%": "#FF763A",
+    "100%": "#FF0000",
+  };
+
+export const wishCountriesPercentageCircleColours: ProgressProps["strokeColor"] =
+  {
+    "0%": "#45FFAA",
+    "100%": "#26E300",
+  };
