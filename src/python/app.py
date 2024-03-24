c4cfbfce4eabc79f2546d336f5ddb4c7c09f237c
Ethan Haugh <90157026+EthanHaugh@users.noreply.github.com>
2024-03-24 15:12:03 +0000

    Add framework for CosmosDB (#8)
    * Add framework for CosmosDB
    
    * Remove test file

--- src/python/app.py
+++ src/python/app.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 from waitress import serve
+from azure.cosmos import CosmosClient
+import os
+
+settings = {
+    'host': os.environ.get('ACCOUNT_HOST', 'https://visited.documents.azure.com:443/'),
+    'master_key': os.environ.get('COSMOS_ACCOUNT_KEY'),
+}
+
+client = CosmosClient(settings['host'], {'masterKey': settings['master_key']}, user_agent="CosmosDBPythonQuickstart", user_agent_overwrite=True)
 
 app = Flask(__name__)
 cors = CORS(app)
 
-
-@app.route('/home')
-def hello():
-    return {'test': 'Hello, World!!'}
-
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=5000)
+
+# Import routes
+from apis import add_location
