69f84aa880dbdabea428a35d0eb96ce1d4991653
Ethan  Haugh <ethanhaugh201@gmail.com>
2024-03-25 22:53:44 +0000

    Add response type to upsertVisitedRecord

--- src/python/queries/location_queries.py
+++ src/python/queries/location_queries.py
@@ -52,7 +52,7 @@ def insertNewRecordForUser(
     return container.create_item(insertInfo)
 
 
-def upsertVisitedRecord(userId: str, arrival: str, departure: str, location: str):
+def upsertVisitedRecord(userId: str, arrival: str, departure: str, location: str) -> Dict[str, any]:
     userRecord = getUserVisitedRecord(userId)
 
     dataToUpsert = dict(
