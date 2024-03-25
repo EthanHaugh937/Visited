588dfeeabb3d7a6e4350e0d2d9128b90a1fc2f6f
Ethan Haugh <90157026+EthanHaugh@users.noreply.github.com>
2024-03-25 22:54:42 +0000

    Rework add_location API (#11)
    * Add validation; insert new; upsert functionality
    
    * Readd authenticated wrapper
    
    * Add response type to upsertVisitedRecord

--- src/python/apis/add_location/add_location.py
+++ src/python/apis/add_location/add_location.py
@@ -1,34 +1,44 @@
-import json
-import uuid
 from flask import jsonify, make_response
-from app import app, client
+from app import app
 
 
 from decorators.decorators import authenticated
+from queries.location_queries import (
+    insertNewRecordForUser,
+    getUserVisitedRecord,
+    upsertVisitedRecord,
+    validateVisitedEntryExists,
+)
 
-db = client.get_database_client("visitedUserTravel")
-container = db.get_container_client("userTravel")
 
-
-@app.route("/test/<string:countryCode>/<string:visitedDate>", methods=["POST"])
+@app.route(
+    "/location/<string:countryCode>/<string:arrival>/<string:departure>",
+    methods=["POST"],
+)
 @authenticated
-def add_location(userId: str, countryCode: str, visitedDate: str):
+def add_location(countryCode: str, arrival: str, departure: str, userId: str = "1234"):
     if not countryCode:
         return make_response(jsonify({"message": "Country Code not provided"}), 404)
 
-    if not visitedDate:
+    if not arrival or not departure:
         return make_response(jsonify({"message": "Visited Date not provided"}), 404)
 
-    if len(list(container.query_items(
-        f"SELECT * FROM userTravel t WHERE t.userId='{userId}' AND t.location='{countryCode}' AND t.date='{visitedDate}'",
-        enable_cross_partition_query=True,
-    ))) != 0:
-        return make_response(jsonify({"message": "Record already exists for that data"}), 404)
-
-    creation_result = container.create_item(
-        dict(
-            id=str(uuid.uuid4()), userId=userId, date=visitedDate, location=countryCode
+    if len(getUserVisitedRecord(userId)) == 0:
+        creation_result = insertNewRecordForUser(
+            userId,
+            arrival,
+            departure,
+            countryCode,
         )
-    )
 
-    return make_response(jsonify(creation_result), 200)
+        return make_response(jsonify(creation_result), 200)
+
+    if validateVisitedEntryExists(
+        userId,
+        arrival,
+        departure,
+        countryCode,
+    ):
+        return make_response(jsonify({"message": "Record already exists"}), 400)
+
+    return upsertVisitedRecord(userId, arrival, departure, countryCode)
---
+++ src/python/queries/location_queries.py
@@ -0,0 +1,66 @@
+from typing import Dict, List
+import uuid
+from app import client
+
+db = client.get_database_client("visitedUserTravel")
+container = db.get_container_client("userTravel")
+
+
+def getUserVisitedRecord(userId: str) -> List[Dict[str, any]]:
+    if item := container.query_items(
+        f"SELECT * FROM userTravel t WHERE t.userId='{userId}'",
+        enable_cross_partition_query=True,
+    ):
+
+        return list(item)
+
+    return []
+
+
+def validateVisitedEntryExists(
+    userId: str, arrival: str, departure: str, locationCode: str
+) -> bool:
+    visitedLocations = getUserVisitedRecord(userId)[0].get("visited")
+
+    recordToSearchFor = {
+        "arrival": arrival,
+        "departure": departure,
+        "location": locationCode,
+    }
+
+    if recordToSearchFor in visitedLocations:
+        return True
+
+    return False
+
+
+def insertNewRecordForUser(
+    userId: str, arrival: str, departure: str, locationCode: str
+) -> Dict[str, str]:
+    insertInfo = {
+        "id": str(uuid.uuid4()),
+        "userId": userId,
+        "visited": [
+            {
+                "arrival": arrival,
+                "departure": departure,
+                "location": locationCode,
+            }
+        ],
+    }
+
+    return container.create_item(insertInfo)
+
+
+def upsertVisitedRecord(userId: str, arrival: str, departure: str, location: str) -> Dict[str, any]:
+    userRecord = getUserVisitedRecord(userId)
+
+    dataToUpsert = dict(
+        arrival= arrival,
+        departure= departure,
+        location= location,
+    )
+
+    userRecord[0]["visited"].append(dataToUpsert)
+
+    return container.upsert_item(body=userRecord[0])
